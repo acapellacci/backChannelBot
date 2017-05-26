09e65fa440f1f499649cfe14b8d057da9791fa73
andrea <andrea.capellacci@3wlab.it>
2017-05-26 12:27:22 +0200

    no message

--- app.js
+++ app.js
@@ -71,17 +71,17 @@ bot.dialog('presentazione', function (session) {
             ]
         });
     session.send('Spero di essere alla sua altezza');
-    waitingFor(5000);
+    waitingFor(10000);
     session.send('Ora ti mostro qualche novità');
-    waitingFor(5000);
+    waitingFor(10000);
     session.send('Per ogni argomento trattato la mia veste grafica cambierà mostrandoti immagini e video attinenti il tema trattato. Per esempio quando parleremo di Corazzieri lo schermo si colorerà di rosso.');
     var reply = createEvent("changeBackground", 'red', session.message.address);
     session.send(reply);
-    waitingFor(5000);
+    waitingFor(10000);
     var reply = createEvent("changeBackground", 'white', session.message.address);
     session.send(reply);
     session.endDialog('Ora non mi rimane che rispondere alle tue domande.');
-    waitingFor(5000);    
+    waitingFor(10000);    
 }).triggerAction({
     matches: 'presentazione'
 });
