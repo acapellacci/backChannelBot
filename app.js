b4120d5eb5f7ecc0914ea84cb18a0f25fb3554ba
andrea <andrea.capellacci@3wlab.it>
2017-05-28 06:21:21 +0200

    no message

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ bot.dialog('chiusura', function (session) {
 });
 
 bot.dialog('faqs', function (session, args, next) {
-    session.endDialog('entities: ' + args.intent.value);
+    session.endDialog('entities: ' + args.intent.score);
 }).triggerAction({
     matches: intents
 });
